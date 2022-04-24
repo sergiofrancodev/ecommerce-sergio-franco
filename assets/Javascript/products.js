f7a88e53b098a75345d8556704308de0925e7782
sergiofrancodev <sergiofrancodev@gmail.com>
2022-04-24 14:43:58 -0500

    Avance de footer y carrrito de compras

--- assets/Javascript/products.js
+++ assets/Javascript/products.js
@@ -2,14 +2,17 @@ let productosCactus = document.getElementById('main-cactus');
 let ventanaCarrito = document.getElementById('carritoDiv');
 let ventanaCarrito1 = document.getElementById('carritoDiv1');
 let clicBoton = document.getElementById('ventanaCarrito');
-let contenedorCarrito = document.getElementById('contenedorCarrito')
+let contenedorCarrito = document.getElementById('contenedorCarrito');
+let contadorCarrito = document.getElementById('contadorCarrito');
+
+
 
 
 /* En este arreglo se van agregando los articulos que se añaden al carrito*/
 let carrito = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('carrito')){
+    if (localStorage.getItem('carrito')) {
         carrito = JSON.parse(localStorage.getItem('carrito'))
         actualizarCarrito()
     }
@@ -19,10 +22,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-stockCactus.forEach((cactus) =>{
+stockCactus.forEach((cactus) => {
     let cardsC = document.createElement('div');
     cardsC.classList.add('cactus');
-    cardsC.innerHTML =`
+    cardsC.innerHTML = `
     <img src=${cactus.img} alt="">
     <div class="descript-card">
     <p class="precio-producto">$${cactus.precio}</p>
@@ -36,78 +39,80 @@ stockCactus.forEach((cactus) =>{
     /* se agrega un div hijo del codigo anterior*/
     productosCactus.appendChild(cardsC)
 
-        /* Con este codigo hacemos que el boton de añadir al carrito */
+    /* Con este codigo hacemos que el boton de añadir al carrito */
 
     let boton = document.getElementById(`Agregar${cactus.id}`)
 
-    boton.addEventListener('click', () =>{
+    boton.addEventListener('click', () => {
 
         addCart(cactus.id)
     })
 
 });
 
+
 let addCart = (prodId) => {
 
-let existe = carrito.some (prod => prod.id ===prodId);
+    let existe = carrito.some(prod => prod.id === prodId)
+    if (existe) {
 
-if(existe){
-    let prod = carrito.map (prod =>{
+       carrito.map(prod => {
 
-        if (prod.id === prodId){
-            prod.cantidad++ 
-        } 
-    })
-} else{
+            if (prod.id === prodId) {
+                prod.cantidad++
+            }
+        })
+    } else {
 
-    let item = stockCactus.find((prod) => prod.id === prodId);
-    carrito.push(item);
+        let item = stockCactus.find((prod) => prod.id === prodId)
+        carrito.push(item)
+        
+    }
+    actualizarCarrito()
+    
+
 
 }
 
-    actualizarCarrito();
-    
-}
-
-
-    
 
 let eliminarDelCarrito = (prodId) => {
 
     let item = carrito.find((prod) => prod.id === prodId)
     let indice = carrito.indexOf(item)
     carrito.splice(indice, 1)
-    actualizarCarrito()
 
+    actualizarCarrito()
 }
+
+
 
 clic = 0;
 
 function mover() {
-    if(clic===0){
+    if (clic === 0) {
 
-        clicBoton.style.left="0";
-        clicBoton.style.transition="all 300ms linear"
-     
+        clicBoton.style.left = "0";
+        clicBoton.style.transition = "all 300ms linear"
+
         clic = clic + 1;
-     
-        } else{
-     
-            clicBoton.style.left="100%";   
-            clicBoton.style.transition="all 300ms linear"  
-     
-         clic =0;
-     
-        }   
+
+    } else {
+
+        clicBoton.style.left = "100%";
+        clicBoton.style.transition = "all 300ms linear"
+
+        clic = 0;
+
+    }
 }
 
 ventanaCarrito.addEventListener('click', mover, true);
 ventanaCarrito1.addEventListener('click', mover, true);
 
-let actualizarCarrito = () =>{
+let actualizarCarrito = () => {
     contenedorCarrito.innerHTML = "";
 
-    carrito.forEach((prod) =>  {
+    carrito.forEach((prod) => {
 
         let div = document.createElement('div');
         div.className = ('productoEnCarrito');
@@ -118,15 +123,14 @@ let actualizarCarrito = () =>{
         <div class="cantidad-cart">${prod.cantidad}</div>
         <div class="precio-cart">$${prod.precio}</div>
         <button onclick ="eliminarDelCarrito(${prod.id})" class="eliminar-del-carrito"> <img src="/assets/icons/trash.svg" alt=""></button></button>
-      
-        
         `
 
         contenedorCarrito.appendChild(div)
-        localStorage.setItem('carrito', JSON.stringify(carrito))
+
     })
+
+    
+        
+    
 }
-
-
-
-
+    
